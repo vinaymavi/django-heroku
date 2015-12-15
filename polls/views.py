4b8f42bd5c85cd5bc6f16f291bc46e928f6c6977
vinay mavi <vinaymavi@gmail.com>
2015-12-15 19:24:31 +0530

    Choice error resolved.

--- polls/views.py
+++ polls/views.py
@@ -1,37 +1,41 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from .models import Question
+from .models import Choice
 
 # Create your views here.
-def index(request):	
-	question_list = Question.objects.order_by('-pub_date')[:5]	
-	template = loader.get_template("polls/index.html")
-	context = RequestContext(request,{'question_list':question_list})
-	return HttpResponse(template.render(context))
+def index(request):
+    question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template("polls/index.html")
+    context = RequestContext(request, {'question_list': question_list})
+    return HttpResponse(template.render(context))
 
-def detail(request,question_id):
-	try:
-		question = Question.objects.get(pk=question_id)
-	except Question.DoesNotExist, e:
-		raise Http404("Question does not exist.")
-	return render(request,'polls/detail.html',{"question":question})
 
-def results(request,question_id):
-	question = get_object_or_404(Question,pk=question_id)
-	return render(request,'polls/results.html',{'question':question})
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist, e:
+        raise Http404("Question does not exist.")
+    return render(request, 'polls/detail.html', {"question": question})
 
-def vote(request,question_id):
-	question = get_object_or_404(Question,pk=question_id)
-	try:
-		selected_choice = question.choice_set.get(pk=request.POST['choice'])
-	except (KeyError,Choice.DoesNotExist):
-		render(request,'polls/detail.html',{"question":question,"error_msg":"Choice not defined."})
-	else:
-		selected_choice.votes +=1
-		selected_choice.save()
-	return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        render(request, 'polls/detail.html', {"question": question, "error_msg": "Choice not defined."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 
