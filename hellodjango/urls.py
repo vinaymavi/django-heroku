798d5b9f1246bee2886c58fcf406336362047757
vinay kumar <vinay.kumar@hsc.com>
2015-12-15 16:55:00 +0530

    Template rendering

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 # TODO serve default url.
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^pools/', include("polls.urls"))
+    url(r'^polls/', include("polls.urls"))
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models import Question
+
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello From! My Poll app..")
+def index(request):	
+	question_list = Question.objects.order_by('-pub_date')[:5]	
+	template = loader.get_template("polls/index.html")
+	context = RequestContext(request,{'question_list':question_list})
+	return HttpResponse(template.render(context))
 
 def detail(request,question_id):
-	return HttpResponse("Looking for question %s" % question_id)
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist, e:
+		raise Http404("Question does not exist.")
+	return render(request,'polls/detail.html',{"question":question})
 
 def results(request,question_id):
 	return HttpResponse("Looking for question %s results" % question_id)
