7ba17b294a7503b89945cf6212d477b4ed34ae8c
vinay(Office) <vinaymavi@gmail.com>
2015-12-29 12:15:59 +0530

    Admin Site:\n Modify default admin site UI and behiviour by implementation of admin.ModelAdmin

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
+class AdminQuestion(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ("Date Information", {'fields': ['pub_date']})
+    ]
+    inlines=[ChoiceInline]
+    list_display=['question_text','pub_date','was_published_recently']
+
+admin.site.register(Question, AdminQuestion)
